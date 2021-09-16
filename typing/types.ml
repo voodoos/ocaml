234076c7efd4b35617a75e5a1d2d98d94f334eff
Ulysse Gérard <thevoodoos@gmail.com>
2021-09-16 16:19:28 +0200

    Move shape out of types

--- typing/types.ml
+++ typing/types.ml
@@ -16,6 +16,7 @@
 (* Representation of types and declarations *)
 
 open Asttypes
+module Uid = Shape.Uid
 
 (* Type expressions for the core language *)
 
@@ -84,209 +85,6 @@ module Private_type_expr = struct
   let set_desc ty d = ty.desc <- d
   let set_level ty lv = ty.level <- lv
   let set_scope ty sc = ty.scope <- sc
-end
-(* *)
-
-module Uid = struct
-  type t =
-    | Compilation_unit of string
-    | Item of { comp_unit: string; id: int }
-    | Internal
-    | Predef of string
-
-  include Identifiable.Make(struct
-    type nonrec t = t
-
-    let equal (x : t) y = x = y
-    let compare (x : t) y = compare x y
-    let hash (x : t) = Hashtbl.hash x
-
-    let print fmt = function
-      | Internal -> Format.pp_print_string fmt "<internal>"
-      | Predef name -> Format.fprintf fmt "<predef:%s>" name
-      | Compilation_unit s -> Format.pp_print_string fmt s
-      | Item { comp_unit; id } -> Format.fprintf fmt "%s.%d" comp_unit id
-
-    let output oc t =
-      let fmt = Format.formatter_of_out_channel oc in
-      print fmt t
-  end)
-
-  let id = ref (-1)
-
-  let reinit () = id := (-1)
-
-  let mk  ~current_unit =
-      incr id;
-      Item { comp_unit = current_unit; id = !id }
-
-  let of_compilation_unit_id id =
-    if not (Ident.persistent id) then
-      Misc.fatal_errorf "Types.Uid.of_compilation_unit_id %S" (Ident.name id);
-    Compilation_unit (Ident.name id)
-
-  let of_predef_id id =
-    if not (Ident.is_predef id) then
-      Misc.fatal_errorf "Types.Uid.of_predef_id %S" (Ident.name id);
-    Predef (Ident.name id)
-
-  let internal_not_actually_unique = Internal
-
-  let for_actual_declaration = function
-    | Item _ -> true
-    | _ -> false
-end
-
-module Shape = struct
-  module Sig_component_kind = struct
-    type t =
-      | Value
-      | Type
-      | Module
-      | Module_type
-      | Extension_constructor
-      | Class
-      | Class_type
-
-    let to_string = function
-      | Value -> "value"
-      | Type -> "type"
-      | Module -> "module"
-      | Module_type -> "module type"
-      | Extension_constructor -> "extension constructor"
-      | Class -> "class"
-      | Class_type -> "class type"
-
-    let can_appear_in_types = function
-      | Value
-      | Extension_constructor ->
-          false
-      | Type
-      | Module
-      | Module_type
-      | Class
-      | Class_type ->
-          true
-  end
-
-  module Item = struct
-    module T = struct
-      type t = string * Sig_component_kind.t
-      let compare = compare
-
-      let value id = Ident.name id, Sig_component_kind.Value
-      let type_ id = Ident.name id, Sig_component_kind.Type
-      let module_ id = Ident.name id, Sig_component_kind.Module
-      let module_type id = Ident.name id, Sig_component_kind.Module_type
-      let extension_constructor id =
-        Ident.name id, Sig_component_kind.Extension_constructor
-    end
-
-    include T
-
-    module Map = Map.Make(T)
-  end
-
-  type var = Ident.t
-  type t =
-    | Var of var
-    | Abs of var * t
-    | App of t * t
-    | Struct of t Item.Map.t
-    | Leaf of Uid.t
-    | Proj of t * Item.t
-    | Comp_unit of string
-
-  let fresh_var =
-    let unique_var_counter = ref 0 in
-    fun () ->
-      unique_var_counter := !unique_var_counter + 1;
-      Printf.sprintf "shape-var-%i" !unique_var_counter
-      |> Ident.create_local
-
-  (* X.Y.Z = Pdot (Pdot (Ident Z, "Y"), "X") -> Proj(Proj(Proj(ShapeX, ("X", mod)), ("Y", mod)), ("Z", mod)) *)
-  let module_proj_of_path ~root path =
-    let rec aux kind path =
-      let kind = Option.value ~default:Sig_component_kind.Module kind in
-      match path with
-      | Path.Pdot (path, name) ->
-        let t = aux None path in
-        Proj (t, (name, kind))
-      | Path.Pident id ->
-        let name = Ident.name id in
-        Proj(root, (name, kind))
-      | Path.Papply _ -> failwith "TODO @ulysse `Papply` not implemented"
-    in
-    aux (Some Sig_component_kind.Module) path
-
-  let make_empty_sig () = Abs(fresh_var (), Struct Item.Map.empty)
-
-  let make_external s = Comp_unit s
-
-  let make_functor ~param body =
-    match param with
-    (* If the functor is generative or has nameless arg, shape is preserved *)
-    | None -> body
-    | Some id -> Abs(id, body)
-
-  let make_functor_app ~arg f = App(f, arg)
-
-  let make_structure shapes = Struct shapes
-
-  let make_coercion ~sig_ mod_ = App(sig_, mod_) (* TODO @ulysse and reduce ? *)
-
-  let reduce_one = function
-    | App _ -> failwith "TODO @ulysse not implemented reduce"
-    | t -> t
-
-  let unproj t =
-    (* TODO @ulysse Write some examples !
-        (for module typeof)
-        Is it right not to go under lambdas ? *)
-    let var = fresh_var () in
-    let rec aux item = function
-      | Struct shapes ->
-        let shapes = Item.Map.mapi
-          (fun item t -> aux (Some item) t) shapes
-        in
-        Struct shapes
-      | Leaf _ as t -> (match item with
-        | None -> t
-        | Some item -> Proj(Var var, item))
-      | (Comp_unit _ | Var _ | Proj _ | Abs _ | App _) as t -> t
-    in
-    Abs(var, aux None t)
-
-  module Map = struct
-    type shape = t
-    type nonrec t = t Item.Map.t
-
-    let add_value t id uid = Item.Map.add (Item.value id) (Leaf uid) t
-    let add_value_proj t id var =
-      let item = Item.value id in
-      Item.Map.add item (Proj(Var var, item)) t
-
-    let add_type t id uid = Item.Map.add (Item.type_ id) (Leaf uid) t
-    let add_type_proj t id var =
-      let item = Item.type_ id in
-      Item.Map.add item (Proj(Var var, item)) t
-
-    let add_module t id uid = Item.Map.add (Item.module_ id) (Leaf uid) t
-    let add_module_proj t id var =
-      let item = Item.module_ id in
-      Item.Map.add item (Proj(Var var, item)) t
-
-    let add_module_type t id shape = Item.Map.add (Item.module_type id) shape t
-    let add_module_type_proj t id var =
-      let item = Item.module_type id in
-      Item.Map.add item (Proj(Var var, item)) t
-
-    let add_extcons t id shape =
-      Item.Map.add (Item.extension_constructor id) shape t
-    let add_extcons_proj t id var =
-      let item = Item.extension_constructor id in
-      Item.Map.add item (Proj(Var var, item)) t
-  end
 end
 
 (* Maps of methods and instance variables *)
@@ -529,7 +327,6 @@ and module_declaration =
     md_attributes: Parsetree.attributes;
     md_loc: Location.t;
     md_uid: Uid.t;
-    md_shape: Shape.t;
   }
 
 and modtype_declaration =
@@ -538,7 +335,6 @@ and modtype_declaration =
     mtd_attributes: Parsetree.attributes;
     mtd_loc: Location.t;
     mtd_uid: Uid.t;
-    mtd_shape: Shape.t;
   }
 
 and rec_status =
--- typing/types.mli
+++ typing/types.mli
@@ -24,6 +24,8 @@
 (** Asttypes exposes basic definitions shared both by Parsetree and Types. *)
 open Asttypes
 
+module Uid = Shape.Uid
+
 (** Type expressions for the core language.
 
     The [type_desc] variant defines all the possible type expressions one can
@@ -248,93 +250,6 @@ module TypeOps : sig
   val compare : t -> t -> int
   val equal : t -> t -> bool
   val hash : t -> int
-end
-
-(* *)
-
-module Uid : sig
-  type t
-
-  val reinit : unit -> unit
-
-  val mk : current_unit:string -> t
-  val of_compilation_unit_id : Ident.t -> t
-  val of_predef_id : Ident.t -> t
-  val internal_not_actually_unique : t
-
-  val for_actual_declaration : t -> bool
-
-  include Identifiable.S with type t := t
-end
-
-module Shape : sig
-  module Sig_component_kind : sig
-    type t =
-      | Value
-      | Type
-      | Module
-      | Module_type
-      | Extension_constructor
-      | Class
-      | Class_type
-
-    val to_string : t -> string
-
-    (** Whether the name of a component of that kind can appear in a type. *)
-    val can_appear_in_types : t -> bool
-  end
-
-  module Item : sig
-    type t
-
-    module Map : Map.S with type key = t
-  end
-
-  type var = Ident.t
-  type t =
-    | Var of var
-    | Abs of var * t
-    | App of t * t
-    | Struct of t Item.Map.t
-    | Leaf of Uid.t
-    | Proj of t * Item.t
-    | Comp_unit of string
-
-  val fresh_var : unit -> var
-
-  val module_proj_of_path : root:t -> Path.t -> t
-
-  val make_empty_sig : unit -> t
-  val make_external : string -> t
-  val make_functor : param:(Ident.t option) -> t -> t
-  val make_functor_app : arg:t -> t -> t
-  val make_structure : t Item.Map.t -> t
-  val make_coercion : sig_:t -> t -> t
-
-  val reduce_one : t -> t
-
-  (** "Reset" a module shape to be used as a module type shape *)
-  val unproj : t -> t
-
-  module Map : sig
-    type shape = t
-    type nonrec t = t Item.Map.t
-
-    val add_value : t -> Ident.t -> Uid.t -> t
-    val add_value_proj : t -> Ident.t -> var -> t
-
-    val add_type : t -> Ident.t -> Uid.t -> t
-    val add_type_proj : t -> Ident.t -> var -> t
-
-    val add_module : t -> Ident.t -> Uid.t -> t
-    val add_module_proj : t -> Ident.t -> var -> t
-
-    val add_module_type : t -> Ident.t -> shape -> t
-    val add_module_type_proj : t -> Ident.t -> var -> t
-
-    val add_extcons : t -> Ident.t -> shape -> t
-    val add_extcons_proj : t -> Ident.t -> var -> t
-  end
 end
 
 (* Maps of methods and instance variables *)
@@ -580,7 +495,6 @@ and module_declaration =
     md_attributes: Parsetree.attributes;
     md_loc: Location.t;
     md_uid: Uid.t;
-    md_shape: Shape.t;
   }
 
 and modtype_declaration =
@@ -589,7 +503,6 @@ and modtype_declaration =
     mtd_attributes: Parsetree.attributes;
     mtd_loc: Location.t;
     mtd_uid: Uid.t;
-    mtd_shape : Shape.t;
   }
 
 and rec_status =
