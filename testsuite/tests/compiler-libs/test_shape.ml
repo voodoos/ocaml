f134e164a832ecb04b539b074b0059c261a9e0ac
Thomas Refis <thomas.refis@gmail.com>
2021-10-01 16:12:32 +0200

    cleanup tests

--- testsuite/tests/compiler-libs/test_shape.ml
+++ testsuite/tests/compiler-libs/test_shape.ml
@@ -34,19 +34,30 @@ module type S = sig type t end
 
 module type Sx = sig
   include S
-  val x : int
+  val x : t
 end
 [%%expect{|
 {
  ("Sx", module type) -> <.3>;
  }
-module type Sx = sig type t val x : int end
+module type Sx = sig type t val x : t end
 |}]
 
-module M : Sx = struct
-  type t
+module M = struct
+  type t = int
   let x = 42
 end
+[%%expect{|
+{
+ ("M", module) -> {
+                   ("t", type) -> <.4>;
+                   ("x", value) -> <.5>;
+                   };
+ }
+module M : sig type t = int val x : int end
+|}]
+
+module M : Sx = M
 [%%expect{|
 {
  ("M", module) -> {
@@ -67,7 +78,17 @@ end
                     ("x", value) -> <.5>;
                     };
  }
-module M' : sig type t = M.t val x : int end
+module M' : sig type t = M.t val x : t end
+|}]
+
+module MS = (M' : S)
+[%%expect{|
+{
+ ("MS", module) -> {
+                    ("t", type) -> <.4>;
+                    };
+ }
+module MS : S
 |}]
 
 module MUnit = struct
@@ -93,57 +114,45 @@ module MUnit :
   end
 |}]
 
-module M'' (X : S) = struct
+module Simple_functor (X : S) = struct
   include X
   type y = X.t
 end
 [%%expect{|
 {
- ("M''", module) ->
-     Abs(X/114, {
-                 ("t", type) -> X/114 . "t"[type];
-                 ("y", type) -> <.10>;
+ ("Simple_functor", module) ->
+     Abs(X/116, {
+                 ("t", type) -> X/116 . "t"[type];
+                 ("y", type) -> <.12>;
                  });
  }
-module M'' : functor (X : S) -> sig type t = X.t type y = X.t end
+module Simple_functor : functor (X : S) -> sig type t = X.t type y = X.t end
 |}]
 
-module M3 (X : S) = struct
+module Simple_functor' (X : S) = struct
   type y = X.t
   include X
 end
 [%%expect{|
 {
- ("M3", module) ->
-     Abs(X/122, {
-                 ("t", type) -> X/122 . "t"[type];
-                 ("y", type) -> <.13>;
+ ("Simple_functor'", module) ->
+     Abs(X/124, {
+                 ("t", type) -> X/124 . "t"[type];
+                 ("y", type) -> <.15>;
                  });
  }
-module M3 : functor (X : S) -> sig type y = X.t type t = X.t end
+module Simple_functor' : functor (X : S) -> sig type y = X.t type t = X.t end
 |}]
+
 module type MFS = functor (X : S) (Y : S) -> sig
   include module type of X
   type u
 end
 [%%expect{|
 {
- ("MFS", module type) -> <.18>;
+ ("MFS", module type) -> <.20>;
  }
 module type MFS = functor (X : S) (Y : S) -> sig type t type u end
-|}]
-
-module type MFS_indir = functor (X : Set.OrderedType) (Y : S) -> sig
-  include module type of X
-  type u
-end
-[%%expect{|
-{
- ("MFS_indir", module type) -> <.22>;
- }
-module type MFS_indir =
-  functor (X : Set.OrderedType) (Y : S) ->
-    sig type t val compare : t -> t -> int type u end
 |}]
 
 module MF : MFS  = functor (X : S) (Y : S) -> struct
@@ -153,66 +162,56 @@ end
 [%%expect{|
 {
  ("MF", module) ->
-     Abs(X/442, Abs(Y/444, {
-                            ("t", type) -> <.25>;
-                            ("u", type) -> <.26>;
+     Abs(X/145, Abs(Y/147, {
+                            ("t", type) -> <.23>;
+                            ("u", type) -> <.24>;
                             }));
  }
 module MF : MFS
 |}]
 
-module type S = sig
-  type t
-  val x : t
-end
-[%%expect{|
-{
- ("S", module type) -> <.30>;
- }
-module type S = sig type t val x : t end
-|}]
 
-module type S1 = functor (X : S) -> sig
+module type S1 = functor (X : Sx) -> sig
   include module type of X
 end
 [%%expect{|
 {
- ("S1", module type) -> <.32>;
+ ("S1", module type) -> <.27>;
  }
-module type S1 = functor (X : S) -> sig type t val x : t end
+module type S1 = functor (X : Sx) -> sig type t val x : t end
 |}]
 
-module type S2 = functor (X : S) -> sig
-  include S
+module type S2 = functor (X : Sx) -> sig
+  include Sx
 end
 [%%expect{|
 {
- ("S2", module type) -> <.34>;
+ ("S2", module type) -> <.29>;
  }
-module type S2 = functor (X : S) -> sig type t val x : t end
+module type S2 = functor (X : Sx) -> sig type t val x : t end
 |}]
 
-module type S3 = functor (X : S) -> S
+module type S3 = functor (X : Sx) -> Sx
 [%%expect{|
 {
- ("S3", module type) -> <.36>;
+ ("S3", module type) -> <.31>;
  }
-module type S3 = functor (X : S) -> S
+module type S3 = functor (X : Sx) -> Sx
 |}]
 
-module F1 (X : S) = struct
+module F1 (X : Sx) = struct
   include X
 end
 [%%expect{|
 {
  ("F1", module) ->
-     Abs(X/481,
+     Abs(X/179,
          {
-          ("t", type) -> X/481 . "t"[type];
-          ("x", value) -> X/481 . "x"[value];
+          ("t", type) -> X/179 . "t"[type];
+          ("x", value) -> X/179 . "x"[value];
           });
  }
-module F1 : functor (X : S) -> sig type t = X.t val x : t end
+module F1 : functor (X : Sx) -> sig type t = X.t val x : t end
 |}]
 
 module Arg = struct
@@ -222,8 +221,8 @@ end
 [%%expect{|
 {
  ("Arg", module) -> {
-                     ("t", type) -> <.39>;
-                     ("x", value) -> <.40>;
+                     ("t", type) -> <.34>;
+                     ("x", value) -> <.35>;
                      };
  }
 module Arg : sig type t = int val x : int end
@@ -232,8 +231,8 @@ module Arg : sig type t = int val x : int end
 include F1 (Arg)
 [%%expect{|
 {
- ("t", type) -> <.39>;
- ("x", value) -> <.40>;
+ ("t", type) -> <.34>;
+ ("x", value) -> <.35>;
  }
 type t = Arg.t
 val x : t = 0
@@ -243,10 +242,10 @@ module F3 = (F1 : S2)
 [%%expect{|
 {
  ("F3", module) ->
-     Abs(X/481,
+     Abs(X/179,
          {
-          ("t", type) -> X/481 . "t"[type];
-          ("x", value) -> X/481 . "x"[value];
+          ("t", type) -> X/179 . "t"[type];
+          ("x", value) -> X/179 . "x"[value];
           });
  }
 module F3 : S2
@@ -255,8 +254,8 @@ module F3 : S2
 include F3 (Arg)
 [%%expect{|
 {
- ("t", type) -> <.39>;
- ("x", value) -> <.40>;
+ ("t", type) -> <.34>;
+ ("x", value) -> <.35>;
  }
 type t = F3(Arg).t
 val x : t = <abstr>
@@ -266,10 +265,10 @@ module F4 = (F1 : S1)
 [%%expect{|
 {
  ("F4", module) ->
-     Abs(X/481,
+     Abs(X/179,
          {
-          ("t", type) -> X/481 . "t"[type];
-          ("x", value) -> X/481 . "x"[value];
+          ("t", type) -> X/179 . "t"[type];
+          ("x", value) -> X/179 . "x"[value];
           });
  }
 module F4 : S1
@@ -278,25 +277,11 @@ module F4 : S1
 include F4 (Arg)
 [%%expect{|
 {
- ("t", type) -> <.39>;
- ("x", value) -> <.40>;
+ ("t", type) -> <.34>;
+ ("x", value) -> <.35>;
  }
 type t = F4(Arg).t
 val x : t = <abstr>
-|}]
-
-module type Foo = sig
-  module type Sig = sig type t end
-  module type Sig_alias = Sig
-
-  include Sig_alias
-end
-[%%expect{|
-{
- ("Foo", module type) -> <.47>;
- }
-module type Foo =
-  sig module type Sig = sig type t end module type Sig_alias = Sig type t end
 |}]
 
 module Coercion : sig
@@ -314,36 +299,21 @@ end
 {
  ("Coercion", module) ->
      {
-      ("E", extension constructor) -> <.51>;
+      ("E", extension constructor) -> <.42>;
       ("M", module) -> {
                         };
-      ("t", type) -> <.49>;
-      ("v", value) -> <.48>;
+      ("t", type) -> <.40>;
+      ("v", value) -> <.39>;
       };
  }
 module Coercion : sig val v : int type t module M : sig end exception E end
 |}]
 
-(* N.B.: the shape of With_alias.A could be the one of Coercion.
-   I guess it's functionally equivalent with the one here, since any use of
-   With_alias to coerce a module M is going to require M.A to be equal to (and
-   share the shape of) Coercion.
-   Still, it's not minimal. *)
-module type With_alias = sig
-  module A = Coercion
-end
-[%%expect{|
-{
- ("With_alias", module type) -> <.58>;
- }
-module type With_alias = sig module A = Coercion end
-|}]
-
 module Fignore(_ : S) = struct let x = 3 end
 [%%expect{|
 {
- ("Fignore", module) -> Abs(shape-var/542, {
-                                            ("x", value) -> <.59>;
+ ("Fignore", module) -> Abs(shape-var/226, {
+                                            ("x", value) -> <.48>;
                                             });
  }
 module Fignore : S -> sig val x : int end
@@ -353,17 +323,17 @@ module Fgen() = struct let x = 3 end
 [%%expect{|
 {
  ("Fgen", module) -> {
-                      ("x", value) -> <.61>;
+                      ("x", value) -> <.50>;
                       };
  }
 module Fgen : functor () -> sig val x : int end
 |}]
 
-module M = Fignore(struct type t = int let x = 3 end)
+module M = Fignore(struct type t = int end)
 [%%expect{|
 {
  ("M", module) -> {
-                   ("x", value) -> <.59>;
+                   ("x", value) -> <.48>;
                    };
  }
 module M : sig val x : int end
@@ -373,7 +343,7 @@ module N = Fgen()
 [%%expect{|
 {
  ("N", module) -> {
-                   ("x", value) -> <.61>;
+                   ("x", value) -> <.50>;
                    };
  }
 module N : sig val x : int end
@@ -384,7 +354,7 @@ class c = object
 end
 [%%expect{|
 {
- ("c", class) -> <.67>;
+ ("c", class) -> <.55>;
  }
 class c : object val v : int end
 |}]
@@ -394,37 +364,18 @@ class type ct = object
 end
 [%%expect{|
 {
- ("ct", class type) -> <.71>;
+ ("ct", class type) -> <.59>;
  }
 class type ct = object val v : int end
 |}]
 
-module type SC = sig
-  class c : object
-    val v : int
-  end
-  class type ct = object
-    val v : int
-  end
-end
-
-[%%expect{|
-{
- ("SC", module type) -> <.74>;
- }
-module type SC =
-  sig
-    class c : object val v : int end
-    class type ct = object val v : int end
-  end
-|}]
 
 module type Fignore = functor (_ : S) -> sig
   val x : int
 end
 [%%expect{|
 {
- ("Fignore", module type) -> <.76>;
+ ("Fignore", module type) -> <.61>;
  }
 module type Fignore = S -> sig val x : int end
 |}]
@@ -434,8 +385,8 @@ module F : Fignore = functor (_ : S) -> struct
 end
 [%%expect{|
 {
- ("F", module) -> Abs(shape-var/626, {
-                                      ("x", value) -> <.77>;
+ ("F", module) -> Abs(shape-var/293, {
+                                      ("x", value) -> <.62>;
                                       });
  }
 module F : Fignore
@@ -453,10 +404,10 @@ module rec A : sig
 [%%expect{|
 {
  ("A", module) -> {
-                   ("t", type) -> <.87>;
+                   ("t", type) -> <.72>;
                    };
  ("B", module) -> {
-                   ("t", type) -> <.89>;
+                   ("t", type) -> <.74>;
                    };
  }
 module rec A : sig type t = Leaf of B.t end
@@ -482,8 +433,8 @@ and B : sig type t = int end
 [%%expect{|
 {
  ("A", module) -> {
-                   ("compare", value) -> <.105>;
-                   ("t", type) -> <.102>;
+                   ("compare", value) -> <.90>;
+                   ("t", type) -> <.87>;
                    };
  ("ASet", module) ->
      {
@@ -684,138 +635,12 @@ and ASet :
   end
 |}]
 
-module type Rec1 = sig
-  module rec A : sig
-    type t = Leaf of B.t
-  end
-
-  and B : sig type t = int end
-end
-[%%expect{|
-{
- ("Rec1", module type) -> <.120>;
- }
-module type Rec1 =
-  sig
-    module rec A : sig type t = Leaf of B.t end
-    and B : sig type t = int end
-  end
-|}]
-
-module type Rec2 = sig
-  module rec A : sig
-    type t = Leaf of string | Node of ASet.t
-    val compare: t -> t -> int
-  end
-
-  and ASet : Set.S with type elt = A.t
-end
-[%%expect{|
-{
- ("Rec2", module type) -> <.133>;
- }
-module type Rec2 =
-  sig
-    module rec A :
-      sig
-        type t = Leaf of string | Node of ASet.t
-        val compare : t -> t -> int
-      end
-    and ASet :
-      sig
-        type elt = A.t
-        type t
-        val empty : t
-        val is_empty : t -> bool
-        val mem : elt -> t -> bool
-        val add : elt -> t -> t
-        val singleton : elt -> t
-        val remove : elt -> t -> t
-        val union : t -> t -> t
-        val inter : t -> t -> t
-        val disjoint : t -> t -> bool
-        val diff : t -> t -> t
-        val compare : t -> t -> int
-        val equal : t -> t -> bool
-        val subset : t -> t -> bool
-        val iter : (elt -> unit) -> t -> unit
-        val map : (elt -> elt) -> t -> t
-        val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
-        val for_all : (elt -> bool) -> t -> bool
-        val exists : (elt -> bool) -> t -> bool
-        val filter : (elt -> bool) -> t -> t
-        val filter_map : (elt -> elt option) -> t -> t
-        val partition : (elt -> bool) -> t -> t * t
-        val cardinal : t -> int
-        val elements : t -> elt list
-        val min_elt : t -> elt
-        val min_elt_opt : t -> elt option
-        val max_elt : t -> elt
-        val max_elt_opt : t -> elt option
-        val choose : t -> elt
-        val choose_opt : t -> elt option
-        val split : elt -> t -> t * bool * t
-        val find : elt -> t -> elt
-        val find_opt : elt -> t -> elt option
-        val find_first : (elt -> bool) -> t -> elt
-        val find_first_opt : (elt -> bool) -> t -> elt option
-        val find_last : (elt -> bool) -> t -> elt
-        val find_last_opt : (elt -> bool) -> t -> elt option
-        val of_list : elt list -> t
-        val to_seq_from : elt -> t -> elt Seq.t
-        val to_seq : t -> elt Seq.t
-        val to_rev_seq : t -> elt Seq.t
-        val add_seq : elt Seq.t -> t -> t
-        val of_seq : elt Seq.t -> t
-      end
-  end
-|}]
-
-module type Std = module type of Unit
-[%%expect{|
-{
- ("Std", module type) -> <.134>;
- }
-module type Std =
-  sig
-    type t = unit = ()
-    val equal : t -> t -> bool
-    val compare : t -> t -> int
-    val to_string : t -> string
-  end
-|}]
-
-module type Std = module type of Stdlib__Unit
-
-[%%expect{|
-{
- ("Std", module type) -> <.135>;
- }
-module type Std =
-  sig
-    type t = unit = ()
-    val equal : t -> t -> bool
-    val compare : t -> t -> int
-    val to_string : t -> string
-  end
-|}]
-
-
-module type MX = module type of M3
-
-[%%expect{|
-{
- ("MX", module type) -> <.136>;
- }
-module type MX = functor (X : S/2) -> sig type y = X.t type t = X.t end
-|}]
-
 module type Small = sig
   type t
 end
 [%%expect{|
 {
- ("Small", module type) -> <.138>;
+ ("Small", module type) -> <.98>;
  }
 module type Small = sig type t end
 |}]
@@ -826,7 +651,7 @@ module type Big = sig
 end
 [%%expect{|
 {
- ("Big", module type) -> <.141>;
+ ("Big", module type) -> <.101>;
  }
 module type Big = sig type t val x : t end
 |}]
@@ -834,7 +659,7 @@ module type Big = sig type t val x : t end
 module type BigToSmall = functor (X : Big) -> Small with type t = X.t
 [%%expect{|
 {
- ("BigToSmall", module type) -> <.144>;
+ ("BigToSmall", module type) -> <.104>;
  }
 module type BigToSmall = functor (X : Big) -> sig type t = X.t end
 |}]
@@ -846,10 +671,10 @@ end
 [%%expect{|
 {
  ("SmallToBig", module) ->
-     Abs(X/1220, {
-                  ("t", type) -> <.146>;
-                  ("x", value) -> <.147>;
-                  });
+     Abs(X/988, {
+                 ("t", type) -> <.106>;
+                 ("x", value) -> <.107>;
+                 });
  }
 module SmallToBig : functor (X : Small) -> sig type t = X.t val x : t end
 |}]
@@ -857,8 +682,8 @@ module SmallToBig : functor (X : Small) -> sig type t = X.t val x : t end
 module F = (SmallToBig : BigToSmall)
 [%%expect{|
 {
- ("F", module) -> Abs(shape-var/1304, {
-                                       ("t", type) -> <.146>;
+ ("F", module) -> Abs(shape-var/1072, {
+                                       ("t", type) -> <.106>;
                                        });
  }
 module F : BigToSmall
