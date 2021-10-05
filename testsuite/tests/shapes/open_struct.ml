5152f4706406adf21a5f98e812e40d36c90d0149
Thomas Refis <thomas.refis@gmail.com>
2021-10-05 14:24:09 +0200

    more things broken

--- testsuite/tests/shapes/open_struct.ml
+++ testsuite/tests/shapes/open_struct.ml
@@ -33,3 +33,67 @@ module N = M
  }
 module N = M
 |}]
+
+(* Not open structs, but the code handling the following is currently very
+   similar to the one for open struct (i.e. calls [Env.enter_signature]), and
+   so we are likely to encounter the same bugs, if any. *)
+
+include struct
+  module M' = struct
+    type t = A
+  end
+end
+[%%expect{|
+{
+ ("M'", module) -> {
+                    ("t", type) -> <.4>;
+                    };
+ }
+module M' : sig type t = A end
+|}]
+
+(* FIXME *)
+module N' = M'
+[%%expect{|
+{
+ ("N'", module) -> {
+                    };
+ }
+module N' = M'
+|}]
+
+module Test = struct
+  module M = struct
+    type t = A
+  end
+end
+[%%expect{|
+{
+ ("Test", module) -> {
+                      ("M", module) -> {
+                                        ("t", type) -> <.8>;
+                                        };
+                      };
+ }
+module Test : sig module M : sig type t = A end end
+|}]
+
+include Test
+[%%expect{|
+{
+ ("M", module) -> {
+                   ("t", type) -> <.8>;
+                   };
+ }
+module M = Test.M
+|}]
+
+(* FIXME *)
+module N = M
+[%%expect{|
+{
+ ("N", module) -> {
+                   };
+ }
+module N = M
+|}]
