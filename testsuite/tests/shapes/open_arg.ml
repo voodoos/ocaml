32b2dc4285db1e36f709c339fc316a18b73c10d8
Ulysse Gérard <thevoodoos@gmail.com>
2021-10-08 14:18:18 +0200

    Update test suite

--- testsuite/tests/shapes/open_arg.ml
+++ testsuite/tests/shapes/open_arg.ml
@@ -10,6 +10,33 @@ end
 ;;
 
 [%%expect{|
-Uncaught exception: Not_found
+{
+ ("Make", module type) -> <.1>;
+ }
+module type Make = functor (I : sig end) -> sig end
+|}]
 
+module Make (I : sig end) : sig
+  open I
+end = struct end
+;;
+
+[%%expect{|
+{
+ ("Make", module) -> Abs(I/93, {
+                                });
+ }
+module Make : functor (I : sig end) -> sig end
+|}]
+
+module type Make = functor (I : sig end) ->
+module type of struct
+  open I
+end
+
+[%%expect{|
+{
+ ("Make", module type) -> <.5>;
+ }
+module type Make = functor (I : sig end) -> sig end
 |}]
