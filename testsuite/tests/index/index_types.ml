49fa211c09ed245eebbf3fb34576f583295301ed
Ulysse Gérard <thevoodoos@gmail.com>
2023-11-13 18:30:01 +0100

    tests: use legacy ocamltest syntaxt

--- testsuite/tests/index/index_types.ml
+++ testsuite/tests/index/index_types.ml
@@ -1,18 +1,18 @@
 (* TEST
-flags = "-bin-annot -store-usage-index";
-compile_only = "true";
-readonly_files = "index_types.ml";
-setup-ocamlc.byte-build-env;
-all_modules = "index_types.ml";
-ocamlc.byte;
-check-ocamlc.byte-output;
-program = "-index -decls index_types.cmt";
-output = "out_objinfo";
-ocamlobjinfo;
-program = "awk '/Indexed/,0' out_objinfo";
-output = "out_awk";
-run;
-check-program-output;
+flags = "-bin-annot -store-usage-index"
+compile_only = "true"
+readonly_files = "index_types.ml"
+* setup-ocamlc.byte-build-env
+** ocamlc.byte
+all_modules = "index_types.ml"
+*** check-ocamlc.byte-output
+**** ocamlobjinfo
+program = "-index -decls index_types.cmt"
+output = "out_objinfo"
+***** run
+program = "awk '/Indexed/,0' out_objinfo"
+output = "out_awk"
+****** check-program-output
 *)
 
 type t = int
