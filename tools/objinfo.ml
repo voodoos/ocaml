0c1c69d14f3a54ab13a6317e1409ede8e52a92d1
Ulysse Gérard <thevoodoos@gmail.com>
2023-12-14 14:05:01 +0100

    item_decls: Reorder+rename the variant's constructors

--- tools/objinfo.ml
+++ tools/objinfo.ml
@@ -145,27 +145,27 @@ let print_cmt_infos cmt =
     printf "\nUid of decls:\n";
     Shape.Uid.Tbl.iter (fun uid item ->
       let loc = match item with
-        | Class_declaration cd -> cd.Typedtree.ci_id_name
-        | Class_description cd -> cd.Typedtree.ci_id_name
-        | Class_type_declaration ctd -> ctd.Typedtree.ci_id_name
-        | Constructor_declaration cd -> cd.Typedtree.cd_name
-        | Extension_constructor ec -> ec.Typedtree.ext_name
-        | Label_declaration ld -> ld.Typedtree.ld_name
-        | Module_binding mb ->
-          Location.{ mb.Typedtree.mb_name with
-            txt = Option.value mb.Typedtree.mb_name.txt ~default:"_" }
-        | Module_declaration md ->
-          Location.{ md.Typedtree.md_name with
-            txt = Option.value md.Typedtree.md_name.txt ~default:"_" }
-        | Module_substitution ms -> ms.Typedtree.ms_name
-        | Module_type_declaration mtd -> mtd.Typedtree.mtd_name
-        | Type_declaration td -> td.Typedtree.typ_name
+        | Value vd -> vd.Typedtree.val_name
         | Value_binding vb ->
           let (_, name, _, _) =
             List.hd (Typedtree.let_bound_idents_full [vb])
           in
           name
-        | Value_description vd -> vd.Typedtree.val_name
+        | Type td -> td.Typedtree.typ_name
+        | Constructor cd -> cd.Typedtree.cd_name
+        | Extension_constructor ec -> ec.Typedtree.ext_name
+        | Label ld -> ld.Typedtree.ld_name
+        | Module md ->
+          { md.Typedtree.md_name with
+            Location.txt = Option.value md.Typedtree.md_name.Location.txt ~default:"_" }
+        | Module_substitution ms -> ms.Typedtree.ms_name
+        | Module_binding mb ->
+          { mb.Typedtree.mb_name with
+            Location.txt =
+              Option.value mb.Typedtree.mb_name.Location.txt ~default:"_" }
+        | Module_type mtd -> mtd.Typedtree.mtd_name
+        | Class cd -> cd.Typedtree.ci_id_name
+        | Class_type ctd -> ctd.Typedtree.ci_id_name
       in
       let pp_loc fmt { Location.txt; loc } =
         Format.fprintf fmt "%s (%a)"
