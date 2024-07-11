0f960ec8b1b3de3a6c382a7ee09a3539bb1ad2db
Ulysse Gérard <thevoodoos@gmail.com>
2024-07-11 14:34:25 +0200

    Fix the tests

--- testsuite/tests/uids/intf_uids.ml
+++ testsuite/tests/uids/intf_uids.ml
@@ -1,23 +1,3 @@
-(* TEST
-
-flags = "-bin-annot";
-compile_only = "true";
-setup-ocamlc.byte-build-env;
-readonly_files = "intf_uids.mli intf_uids.ml";
-all_modules = "intf_uids.mli intf_uids.ml";
-ocamlc.byte;
-check-ocamlc.byte-output;
-
-program = "-quiet -decls intf_uids.cmti intf_uids.cmt";
-output = "out_objinfo";
-ocamlobjinfo;
-
-check-program-output;
-*)
-
-(* This test illustrates the fact that uids are tagged to indicate if they
-  originate from an interface of an implementation. *)
-
 type u (* has uid Intf_uids.0 *)
 
 type t (* has uid Intf_uids.1 *)
