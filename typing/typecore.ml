9b217e9685e7afbef4dba93f7b413fff6f10367e
Ulysse Gérard <thevoodoos@gmail.com>
2025-04-02 18:08:18 +0200

    Revert "Wrap and generalize filter_arrow_mono if principal"
    This reverts commit ca118b6f6aa9aa8a37708bbc50c969360e8969b5.

--- typing/typecore.ml
+++ typing/typecore.ml
@@ -5894,8 +5894,7 @@ and type_application env funct sargs =
   | (* Special case for ignore: avoid discarding warning *)
     [Nolabel, sarg] when is_ignore funct ->
       let { ty_arg; ty_ret } =
-        with_local_level_generalize_structure_if_principal (fun () ->
-          filter_arrow_mono env (instance funct.exp_type) Nolabel)
+        filter_arrow_mono env (instance funct.exp_type) Nolabel
       in
       let exp = type_expect env sarg (mk_expected ty_arg) in
       check_partial_application ~statement:false exp;
